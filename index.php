c834efb80f82f0cb7d315badc3754029be6af122
David Grudl <david@grudl.com>
2013-06-30 23:04:32 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+function adminer_object() {
+    // required to run any plugin
+    include_once dirname(__FILE__) . '/plugins/plugin.php';
+
+    // autoloader
+    foreach (glob(dirname(__FILE__) . '/plugins/*.php') as $filename) {
+        include_once $filename;
+    }
+
+    $plugins = array(
+    );
+
+    /* It is possible to combine customization and plugins:
+    class AdminerCustomization extends AdminerPlugin {
+    }
+    return new AdminerCustomization($plugins);
+    */
+
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include dirname(__FILE__) . '/adminer.php';
