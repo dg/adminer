766d9d985e013801ecdb3bc4b7e508397ace88e3
David Grudl <david@grudl.com>
2014-01-10 01:20:19 +0100

    typos

--- index.php
+++ index.php
@@ -5,26 +5,17 @@ if (@$_GET['file'] === 'default.css') {
 }
 
 function adminer_object() {
-    // required to run any plugin
-    include_once dirname(__FILE__) . '/plugins/plugin.php';
+	include_once dirname(__FILE__) . '/plugins/plugin.php';
 
-    // autoloader
-    foreach (glob(dirname(__FILE__) . '/plugins/*.php') as $filename) {
-        include_once $filename;
-    }
+	foreach (glob(dirname(__FILE__) . '/plugins/*.php') as $filename) {
+		include_once $filename;
+	}
 
-    $plugins = array(
-        new AdminerAutocomplete,
-    );
+	$plugins = array(
+		new AdminerAutocomplete,
+	);
 
-    /* It is possible to combine customization and plugins:
-    class AdminerCustomization extends AdminerPlugin {
-    }
-    return new AdminerCustomization($plugins);
-    */
-
-    return new AdminerPlugin($plugins);
+	return new AdminerPlugin($plugins);
 }
 
-// include original Adminer or Adminer Editor
 include dirname(__FILE__) . '/adminer.php';
