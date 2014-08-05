9365952e11b2b706be94c5000bf032027557a45b
David Grudl <david@grudl.com>
2014-08-05 18:31:58 +0200

    all static files are loaded via index.php

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 
-if (@$_GET['file'] === 'default.css') {
-	header('Content-Type: text/css');
-	return;
+if (empty($_GET['file'])) {
+	ob_start(function($s) {
+		return preg_replace_callback('#(<(link|script)\s[^>]*(href|src)=")(adminer\.css|static/.+)"#U', function($m) {
+			return $m[1] . '?file=' . urlencode($m[4]) . '"';
+		}, $s);
+	}, 4096);
+
+} elseif (preg_match('#^(default|adminer|static(/\w[\w.-]*)+)\.(\w+)\z#', $_GET['file'], $m)) {
+	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+		header('HTTP/1.1 304 Not Modified');
+		exit;
+	}
+
+	header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime('1 month')) . ' GMT');
+	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+
+	$types = array('css' => 'text/css', 'js' => 'text/javascript', 'gif' => 'image/gif', 'png' => 'image/png');
+	if (isset($types[$m[3]])) {
+		header('Content-Type: ' . $types[$m[3]]);
+	}
+	@readfile(__DIR__ . '/' . $_GET['file']);
+	exit;
 }
+
 
 function adminer_object() {
 	include_once dirname(__FILE__) . '/plugins/plugin.php';
