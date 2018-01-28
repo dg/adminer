cdb8455a95739812f3d6a58ae207e7770d2fcacf
Daniel Mejta <daniel@mejta.net>
2018-01-28 11:01:24 +0100

    Fix of adminer upgrade to 4.5.0
    we have to use scripts with nonce, see https://sourceforge.net/p/adminer/news/2018/01/adminer-440-released/ and https://sourceforge.net/p/adminer/news/2018/01/adminer-450-released/

--- plugins/disableJush.php
+++ plugins/disableJush.php
@@ -10,7 +10,7 @@ class AdminerDisableJush
 	public function head()
 	{
 		?>
-<script type="text/javascript">
+<script<?php echo nonce();?> type="text/javascript">
 (function(){
 	var origBodyLoad = bodyLoad,
 		tags = document.getElementsByTagName('textarea');
--- plugins/remoteColor.php
+++ plugins/remoteColor.php
@@ -10,7 +10,7 @@ class AdminerRemoteColor
 	{
 		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) ||
 			!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true)) {
-			echo '<script>document.documentElement.className+=" remote";</script>';
+			echo '<script' . nonce() . '>document.documentElement.className+=" remote";</script>';
 		}
 	}
 }
