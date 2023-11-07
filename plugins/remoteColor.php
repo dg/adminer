8ba1be7a43e59729f18b52cd9d2f19bc2dc41b50
David Grudl <david@grudl.com>
2023-11-07 18:15:15 +0100

    remote-colors: detects remove mysql

--- plugins/remoteColor.php
+++ plugins/remoteColor.php
@@ -8,8 +8,12 @@ class AdminerRemoteColor
 {
 	public function head()
 	{
-		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) ||
-			!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true)) {
+		$localhosts = ['127.0.0.1', '::1', 'localhost'];
+		$connection = connection();
+		if (
+			(isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) || !in_array($_SERVER['REMOTE_ADDR'], $localhosts, true))
+			|| ($connection instanceof MySQLi && !in_array(explode(' ', $connection->host_info)[0], $localhosts, true))
+		) {
 			echo '<script' . nonce() . '>document.documentElement.className+=" remote";</script>';
 		}
 	}
