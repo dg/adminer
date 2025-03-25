becaf7c76f0b3fc6c8d607bcf64ecf8322991d23
David Grudl <david@grudl.com>
2025-03-25 12:37:09 +0100

    uses plugin autoloading
    (removes support for ENV login)

--- adminer-plugins/login-without-credentials.php
+++ adminer-plugins/login-without-credentials.php
@@ -10,6 +10,10 @@ class AdminerLoginWithoutCredentials
 {
 	public function login()
 	{
-		return true;
+		$local = !isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+			&& isset($_SERVER['REMOTE_ADDR'])
+			&& in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true);
+
+		return $local ? true : null;
 	}
 }
