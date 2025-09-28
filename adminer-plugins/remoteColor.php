909b7863f79d2466f884f9bc109b15c008a48211
Michal Špaček <mail@michalspacek.cz>
2025-09-28 05:08:27 +0200

    Lowercase the host_info in remote address check
    In my case host_info returns "Localhost via UNIX socket" meaning "Localhost" won't be found in the $localhosts array.
    
    "Localhost" is also mentioned in https://www.php.net/manual/en/mysqli.get-host-info.php in the example output.

--- adminer-plugins/remoteColor.php
+++ adminer-plugins/remoteColor.php
@@ -13,7 +13,7 @@ class AdminerRemoteColor
 		$connection = Adminer\connection();
 		if (
 			(isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) || !in_array($_SERVER['REMOTE_ADDR'], $localhosts, true))
-			|| ($connection instanceof mysqli && !in_array(explode(' ', $connection->host_info)[0], $localhosts, true))
+			|| ($connection instanceof mysqli && !in_array(strtolower(explode(' ', $connection->host_info)[0]), $localhosts, true))
 		) {
 			echo '<script' . Adminer\nonce() . '>document.documentElement.className+=" remote";</script>';
 		}
